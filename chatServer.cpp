9550a0a48f8958483f0f50490533cd777702a4cc
Leifur Pálsson <leifur@ru.is>
2018-09-21 19:44:49 +0000

    added updated chatserver

--- chatServer.cpp
+++ chatServer.cpp
@@ -1,27 +1,39 @@
+/* helpful links
+https://linux.die.net/man/2/socket
+
+client and server example:
+http://www.linuxhowtos.org/C_C++/socket.htm
+
+http://www.gnu.org/software/libc/manual/html_node/Server-Example.html
+*/
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <iostream>
-#include <string>
 #include <array>
+#include <string.h> // for bzero
+#include <fstream> // fpr read
+
+#include <arpa/inet.h> //inet_ntoa
+
+#include <stdlib.h>
+#include <unistd.h> // close()
 
 
-#include <ctime> //For the timestamp
+#include <ctime> // for the timestamp
 #include <sstream>
 #include <iomanip>
 
 using namespace std;
 
-
-void error(char *msg)
-{
+void error(const char *msg) {
     perror(msg);
     exit(1);
 }
 
-int main(int argc, char *argv[])
-{
+string provide_unique_id() {
     //group = Y_Project_2 42 Y_Project_2
     int sockFd;
     struct sockaddr_in serverAddress;
@@ -31,55 +43,120 @@ int main(int argc, char *argv[])
 
     FILE* pipe = popen(command.c_str(), "r");
 
-
     if (!pipe) {
        std::cerr << "Couldn't start command." << std::endl;
        return 0;
     }
-   while (fgets(buffer.data(), 128, pipe) != NULL) {
+    while (fgets(buffer.data(), 128, pipe) != NULL) {
        std::cout << "Reading..." << std::endl;
        result += buffer.data();
-   }
+    }
 
-  auto t = std::time(nullptr);
-  auto tm = *std::localtime(&t);
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
 
-  std::ostringstream oss;
-  oss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
-  auto str = oss.str();
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
+    auto str = oss.str();
 
-  result = result + " Y_Project_2 42" + str;
-  cout<< "result: "<< result << endl;
+    result = result + " Y_Project_2 42" + str;
+    return result;
+}
 
-     /*int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");*/
-     return 0;
+int createSocket(int socketFileDescriptor) {
+    socketFileDescriptor = socket(AF_INET, SOCK_STREAM|SOCK_NONBLOCK, 0);
+    if (socketFileDescriptor < 0) {
+         error("ERROR opening socket");
+    }
+    return socketFileDescriptor;
+}
+
+int bindSocket(int socketFileDescriptor, struct sockaddr_in serverAddress) {
+    int bindFD1 = bind(socketFileDescriptor, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+    if(bindFD1 < 0) {
+      error("Error binding socket");
+    }
+    return bindFD1;
+}
+
+int selectFileDescritporSet(fd_set FDSet) {
+    int selectDescriptor = select(FD_SETSIZE, &FDSet, NULL, NULL, NULL);
+    if (selectDescriptor < 0) {
+        error("Error selecting");
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    cout << provide_unique_id();
+
+    //select 3 consecutive ports for port knocking
+    int portNumber1 = 50040;
+    int portNumber2 = 50041;
+    int portNumber3 = 50042;
+
+    // initalize File Descriptors
+    int socketFD1 = 0;
+    //int socketFileDescriptor2 = 0;
+    //int socketFileDescriptor3 = 0;*/
+
+    int bindFD1 = 0;
+
+    int newSocketFD1 = 0;
+
+    // create set of file descritor sets
+    fd_set activeFDSet;
+    fd_set readFDSet;
+
+    int returnValue = 0; // return value for the read() and write() calls; i.e. it contains the number of characters read or written.
+    socklen_t clilen = 0; // stores the size of the address of the client. This is needed for the accept system call.
+
+    struct sockaddr_in serverAddress;
+    struct sockaddr_in clientAddress;
+
+    createSocket(socketFD1);
+
+    bzero((char *) &serverAddress, sizeof(serverAddress)); // initializes serverAddress to zeros.
+    serverAddress.sin_family = AF_INET; // code for address family, this is always set to AF_INET
+    serverAddress.sin_addr.s_addr = INADDR_ANY; // this will always be the IP address of the machine on which the server is running
+    serverAddress.sin_port = portNumber1;
+
+    bindFD1 = bindSocket(socketFD1, serverAddress);
+
+    // Initialize the set of active sockets.
+    FD_ZERO (&activeFDSet);
+    FD_SET (socketFD1, &activeFDSet);
+
+    while(1) {
+        // Block untill input arrives on one or more of the active sockets
+        readFDSet = activeFDSet;
+        for(int i = 0; i < FD_SETSIZE; i++) {
+            // check if file descriptor is part of the set
+            if(FD_ISSET(i, &readFDSet)) {
+                if(i == socketFD1) {
+                    clilen = sizeof(clientAddress);
+                    /*extract the first connection on the queue of pending connections,
+                    create a new socket with the same socket type protocol
+                    and address family as the specified socket,
+                    and allocate a new file descriptor for that socket.*/
+                    newSocketFD1 = accept(socketFD1, (struct sockaddr *) &clientAddress, &clilen);
+                    if(newSocketFD1 < 0) {
+                        error("accept errror");
+                    }
+
+                    cout << stderr << "Server: connect from host %s, port %hd." << endl;
+                    cout << inet_ntoa (clientAddress.sin_addr) << clientAddress.sin_port << endl;
+                }
+                else {
+                    // data arriving on a connected socket
+                    //if (read_from_client (i) < 0) {
+                        close (i);
+                        FD_CLR (i, &activeFDSet);
+                    //}
+                }
+            }
+        }
+    }
+
+    return 0;
 }
